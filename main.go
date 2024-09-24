eee1861ae5640bfa41b92adbba0c6e77e5140281
Oleksii <onk230@aubg.bg>
2024-09-24 15:32:04 +0300

    трохи поворкав

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import "fmt"
 
 func main() {
 	newblockchain := NewBlockchain()
-	newBlockchain.AddBlock("first transaction")
-	newBlockchain.AddBlock("second transaction")
+	newblockchain.AddBlock("first transaction")
+	newblockchain.AddBlock("second transaction")
+	newblockchain.AddBlock("third transaction")
 	for _, block := range newblockchain.Blocks {
 		fmt.Printf("Hash of the block %x\n", block.MyBlockHash)                 // print the hash of the block
 		fmt.Printf("Hash of the previous Block: %x\n", block.PreviousBlockHash) // print the hash of the previous block
